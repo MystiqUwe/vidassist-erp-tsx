feff38460595279acfa64c3b7642639547135682
Beni <vizedgraphics@googlemail.com>
2023-12-30 23:19:29 +0100

    change: user-nav, user-nav-client
    user-nav aufgeteilt auf server und client seite für die session

--- vidassist-erp-tsx/app/(dashboard)/dashboard/process-videos/[videoId]/page.tsx
+++ vidassist-erp-tsx/app/(dashboard)/dashboard/process-videos/[videoId]/page.tsx
@@ -1,10 +1,37 @@
+"use client";
+
+import BreadCrumb from "@/components/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import MuxPlayer from "@mux/mux-player-react";
+import { Trash } from "lucide-react";
+
 const ViewVideo = ({ params }: { params: { videoId: string } }) => {
   //Retrieve Video from database
-
+  const playbackId = "MiDSq019ZVmHWC9YKvc1GpOMgZ6jmrGRNMPXbfRtuzH00";
+  const breadcrumbItems = [
+    { title: "Process videos", link: "/dashboard/process-videos" },
+    { title: "Watch", link: `dashboard/process-videos/${params.videoId}` },
+  ];
   return (
-    <div>
-      <h1>View Video {params.videoId}</h1>
-    </div>
+    <>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BreadCrumb items={breadcrumbItems} />
+        <div className="flex items-center justify-between">
+          <Heading title={"View Video"} description={"Watch the video"} />
+        </div>
+        <Separator />
+        <div>
+          <MuxPlayer
+            streamType="on-demand"
+            style={{ width: "100%" }}
+            playbackId={playbackId}
+            metadata={{ player_name: "with-mux-video" }}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
--- vidassist-erp-tsx/components/layout/user-nav.tsx
+++ vidassist-erp-tsx/components/layout/user-nav.tsx
@@ -1,72 +1,8 @@
-"use client";
-import { logout } from "@/app/auth/actions";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTransition } from "react";
-export function UserNav() {
-  const [isPending, startTransition] = useTransition();
-  function onSignOut() {
-    startTransition(async () => {
-      await logout();
-    });
-  }
+import { readUserSession } from "@/lib/actions";
+import UserNavClient from "./user-nav-client";
 
-  //retrieve the user session. Keep in mind that this is a client component
-
-  const session = true;
-  if (session) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src={"" ?? ""} alt={"" ?? ""} />
-              <AvatarFallback>{"bsd"}</AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{"name"}</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {"email"}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Profile
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Billing
-              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Settings
-              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>New Team</DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => onSignOut()}>
-            Log out
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
+export async function UserNav() {
+  const { data: userSession } = await readUserSession();
+  console.log("session", userSession.session);
+  return <UserNavClient email={userSession.session.user.email} />;
 }
